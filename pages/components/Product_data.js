ab3b29b11a3a761acfb4281415c3ec8da0f6e6c1
sumanmedda <sumanmedda33@gmail.com>
2024-08-23 12:16:46 +0530

    updates

--- pages/components/Product_data.js
+++ pages/components/Product_data.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function ProductData({ product, index }) {
@@ -8,25 +9,28 @@ export default function ProductData({ product, index }) {
           <th class=" px-6 py-4 font-small text-gray-900">
             {index + 1}
             <div class="relative h-7 w-10">
-              <img
+              <Image
                 class="h-10 w-10 rounded-full object-cover object-center"
-                src={product.images}
+                src={product?.images}
                 alt=""
               />
             </div>
           </th>
-          <td class="px-6 py-4">{product.title}</td>
-          <td class="px-6 py-4 max-w-xs">{product.description}</td>
-          <td class="px-6 py-4">Rs {product.price}</td>
+          <td class="px-6 py-4">{product?.title}</td>
+          <td class="px-6 py-4 max-w-xs">{product?.description}</td>
+          <td class="px-6 py-4">Rs {product?.price}</td>
 
           <td class="flex justify-end gap-4 px-6 py-4 font-medium">
             <Link
-              href={"/products/delete/" + product._id}
+              href={"/products/delete/" + product?._id}
               className="text-red-500"
             >
               Delete
             </Link>
-            <Link href={"/products/edit/" + product._id} class="text-green-400">
+            <Link
+              href={"/products/edit/" + product?._id}
+              class="text-green-400"
+            >
               Edit
             </Link>
           </td>
